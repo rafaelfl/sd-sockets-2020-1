c1c7f38af8572c489cf38e57978a9a6944de38fe
Rafael Fernandes Lopes <rafael.fernandes@gmail.com>
2020-10-21 19:33:06 -0300

    Implementado um servidor de geração de números aleatórios

--- client.js
+++ client.js
@@ -3,12 +3,15 @@ const net = require('net');
 const client = net.Socket();
 
 const clientConnected = () => {
-    console.log("Cliente conectado");
+    // console.log("Cliente conectado");
 
+    client.write("NUMERO_ALEATORIO 10");
+    
     client.on("data", (data) => {
-        console.log("===> ");
-        console.log(data.toString());
+        console.log("Número aleatório: " + data.toString());
+    
+        client.write("FINALIZA");
     });
 }
 
-client.connect(3000, "127.0.0.1", clientConnected)
+client.connect(3000, "www.ecp.ufma.br", clientConnected)
--- server.js
+++ server.js
@@ -1,8 +1,36 @@
-const net = require('net');
+const net = require("net");
 
 const connectionListener = (socket) => {
-    socket.write("Ola! Bem vindo ao sistema de mensagens. Digite uma letra e eu vou te mandar uma mensagem do dia");
 
+    socket.on("data", (data) => {
+        const comando = data.toString();
+        console.log(comando);
+
+        const parametros = comando.split(" ");
+        console.log(parametros);
+
+        if (parametros[0] === "NUMERO_ALEATORIO") {
+            let valorMaximo;
+
+            if (parametros.length > 1) {
+                valorMaximo = parseInt(parametros[1]);
+            } else {
+                valorMaximo = 100;
+            }
+
+            const valorAleatorio = Math.floor(Math.random() * valorMaximo) + 1;
+
+            socket.write(valorAleatorio.toString());
+        } else if (parametros[0] === "FINALIZA") {
+            socket.end();
+        } else {
+            socket.write("COMANDO_INVALIDO");
+        }
+    });
+
+    socket.on("end", () => {
+        console.log("Cliente desconectado!");
+    });
 }
 
 const server = net.createServer(connectionListener);
